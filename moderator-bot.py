e31ff7597be5a749255a0ce777a9eda83df0ad25
gc-plp <plp.github@gmail.com>
2018-09-23 20:42:11 +0000

    Update bot entrypoint

--- moderator-bot.py
+++ moderator-bot.py
@@ -1,16 +1,27 @@
 import praw
 import os
 import threading
+import configparser
 from modbot.plugin import plugin_manager
-
-threads = []
-subreddits = ['romania']
 
 USER_AGENT="Romania moderator bot by /u/programatorulupeste"
 reddit = praw.Reddit("ro_moderator_bot", user_agent=USER_AGENT)
 
+# Load configs
+config = configparser.ConfigParser()
+config.read("reddit-bot.ini")
+
+# Check if the config wants us to enable plugin reloading
+with_reload = False
+if "debug" in config and "reload" in config["debug"]:
+    with_reload = True
+
+# Parse the subreddit list to watch
+subreddits = config["config"]["watch_subs"].split(",")
+subreddits = map(str.strip, subreddits)
+
 # Load plugins
-pmgr = plugin_manager([os.path.abspath("plugins")], reddit)
+pmgr = plugin_manager([os.path.abspath("plugins")], reddit, with_reload, config)
 pmgr.watch_subs(subreddits)
 
 while True:
