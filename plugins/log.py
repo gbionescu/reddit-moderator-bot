dda69143ede00760b46270564ac46d850bcf0a05
gc-plp <plp.github@gmail.com>
2019-06-11 10:31:52 +0300

    Add logger plugin

--- plugins/log.py
+++ plugins/log.py
@@ -2,8 +2,10 @@ from modbot import hook
 
 @hook.submission()
 def call_new_sub(db, submission):
+    print("new sub")
     db.add_submission(submission)
 
 @hook.comment()
 def call_new_comment(comment, db):
+    print("new comment")
     db.add_comment(comment)
