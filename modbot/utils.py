be28590271c0bf9a189319c9eb0137fa6691ee0e
gc-plp <plp.github@gmail.com>
2019-10-28 02:04:21 +0000

    Add SEC_IN_DAY

--- modbot/utils.py
+++ modbot/utils.py
@@ -5,6 +5,8 @@ class timedata:
     MIN_IN_HOUR = 60
     HOUR_IN_DAY = 24
 
+    SEC_IN_DAY = SEC_IN_MIN * MIN_IN_HOUR * HOUR_IN_DAY
+
 def utcnow():
     return (datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds()
 
