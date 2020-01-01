e9a5ffae9a4209e78bf1467afa70afb23f904c8c
gc-plp <plp.github@gmail.com>
2020-01-01 14:14:24 +0000

    Change format of system status

--- plugins/system.py
+++ plugins/system.py
@@ -14,15 +14,15 @@ def system_status(message, ):
     # get the data we need using the Process we got
     cpu_usage = process.cpu_percent(1)
     thread_count = process.num_threads()
-    memory_usage = process.memory_info()[0]
+    memory_usage = process.memory_info()[0] / (1024 * 1024)
     uptime = timedelta(seconds=round(time.time() - process.create_time()))
 
     reply = \
     """
-    Uptime: %s\n\n
-    Threads: %s\n\n
-    CPU Usage: %s\n\n
-    Memory Usage: %s\n\n""" % (
+    Uptime: %s
+    Threads: %s
+    CPU Usage: %s
+    Memory Usage (MB): %s""" % (
         uptime,
         thread_count,
         cpu_usage,
