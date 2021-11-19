3c6a74cb980bd9e25ffc2102db2bfdf83dd49828
gc-plp <plp.github@gmail.com>
2021-11-19 17:28:03 +0100

    debug.sh -> run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,4 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 "$DIR/build.sh"
 
-docker run -it --rm --cpuset-cpus 0 --net=host -v $DIR/..:/botsrc redditmodbot /debug.sh "$@"
+docker run -it --rm --cpuset-cpus 0 --net=host -v $DIR/..:/botsrc redditmodbot /run.sh "$@"
