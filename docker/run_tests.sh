02922b4481755ad5cf58c2f04ecc93fe0fe1b97e
gc-plp <plp.github@gmail.com>
2021-11-19 17:39:04 +0100

    run tests through dedicated entrypoint

---
+++ docker/run_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+"$DIR/build.sh"
+
+docker run --rm --cpuset-cpus 0 --net=host -v $DIR/..:/botsrc redditmodbot /run.sh "pytest"
